b59bd4446e694177ed39ced0208c9fb8ae24f1d5
glokta1 <rafeyahmad@protonmail.com>
2023-05-08 23:36:11 +0530

    chore: formatting and cleanup

--- api/api.go
+++ api/api.go
@@ -1,16 +1,12 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+// func LaunchServer() {
+//     app := fiber.New()
 
-func LaunchServer() {
-    app := fiber.New()
+//     app.Get("/api/upcoming", func(c *fiber.Ctx) error {
+//     })
 
-    app.Get("/api/upcoming", func(c *fiber.Ctx) error {
-        contests := g 
-    })
+//     app.Listen(":3000")
+// }
 
-    app.Listen(":3000")
-}
-
-func main
-
+// func main
--- main.go
+++ main.go
@@ -1,105 +1,100 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
-	"os"
+	// "os"
 	"strconv"
 	"strings"
 	"time"
-    "github.com/gofiber/fiber/v2"
+
 	"github.com/gocolly/colly/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
 type Contest struct {
-	Name      string `json:"name"` 
-    Duration  int `json:"duration"`
-	StartTime int64 `json:"startTime"`
-	Link       string `json:"link"`
+	Name      string `json:"name"`
+	Duration  int    `json:"duration"`
+	StartTime int64  `json:"startTime"`
+	Link      string `json:"link"`
 }
 
-
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/api/upcoming", func(c *fiber.Ctx) error {
-        contests := getUpcomingContests()
-        return c.JSON(contests)
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("This is the unofficial Atcoder API")
+	})
 
-    app.Listen(":3000")
+	app.Get("/api/upcoming", func(c *fiber.Ctx) error {
+		contests := getUpcomingContests()
+		return c.JSON(contests)
+	})
+
+	app.Listen(":3000")
 }
 
 func getUpcomingContests() []Contest {
-    c := colly.NewCollector()
-    contests := make([]Contest, 0)
-        
-    c.OnRequest(func(r *colly.Request) {
-        fmt.Println("Visiting", r.URL)
-    })
+	c := colly.NewCollector()
+	contests := make([]Contest, 0)
 
-    c.OnHTML("div[id=contest-table-upcoming] > div.panel > div.table-responsive > table.table > tbody", func(h *colly.HTMLElement) {
-        h.ForEach("tr", func(i int, h *colly.HTMLElement) {
-            name := h.DOM.Find(":nth-child(2) > a").Text()
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
 
-            baseURL := "https://atcoder.jp"
-            contestPath, exists:= h.DOM.Find(":nth-child(2) > a").Attr("href")
-            if !exists {
-                fmt.Println("href attribute does not exist")
-            }
-            link := baseURL + contestPath
+	c.OnHTML("div[id=contest-table-upcoming] > div.panel > div.table-responsive > table.table > tbody", func(h *colly.HTMLElement) {
+		h.ForEach("tr", func(i int, h *colly.HTMLElement) {
+			name := h.DOM.Find(":nth-child(2) > a").Text()
 
-            datetime := h.DOM.Find(":nth-child(1) > a > time.fixtime-full").Text()
-            // reference time written in datetime's format for later parsing
-            datetimeLayout := "2006-01-02 15:04:05-0700"
-            t, err := time.Parse(datetimeLayout, datetime)
-            if err != nil {
-                fmt.Println(err)
-            }
-            startTime := t.Unix()
+			baseURL := "https://atcoder.jp"
+			contestPath, exists := h.DOM.Find(":nth-child(2) > a").Attr("href")
+			if !exists {
+				fmt.Println("href attribute does not exist")
+			}
+			link := baseURL + contestPath
 
-            durationString := h.DOM.ChildrenFiltered(":nth-child(3)").Text()
-            duration := parseDuration(durationString)
+			datetime := h.DOM.Find(":nth-child(1) > a > time.fixtime-full").Text()
+			// reference time written in datetime's format for later parsing
+			datetimeLayout := "2006-01-02 15:04:05-0700"
+			t, err := time.Parse(datetimeLayout, datetime)
+			if err != nil {
+				fmt.Println(err)
+			}
+			startTime := t.Unix()
 
-            // initialize Contest struct var with parsed values
-            contest := Contest{
-                Name: name,
-                Duration: duration,
-                StartTime: startTime,
-                Link: link,
-            }
+			durationString := h.DOM.ChildrenFiltered(":nth-child(3)").Text()
+			duration := parseDuration(durationString)
 
-            contests = append(contests, contest)
-        })
-    })
+			// initialize Contest struct var with parsed values
+			contest := Contest{
+				Name:      name,
+				Duration:  duration,
+				StartTime: startTime,
+				Link:      link,
+			}
 
-    c.Visit("https://atcoder.jp/contests/")
+			contests = append(contests, contest)
+		})
+	})
 
-    return contests
+	c.Visit("https://atcoder.jp/contests/")
+
+	return contests
 }
 
 // converts duration specified in HH:MM to seconds
 func parseDuration(durationString string) int {
-    parts := strings.Split(durationString, ":")
-    hours, err := strconv.Atoi(parts[0])
-    if err != nil {
-        fmt.Println("Couldn't parse hours")
-    }
+	parts := strings.Split(durationString, ":")
+	hours, err := strconv.Atoi(parts[0])
+	if err != nil {
+		fmt.Println("Couldn't parse hours")
+	}
 
-    minutes, err := strconv.Atoi(parts[1])
-    if err != nil {
-        fmt.Println("Couldn't parse minutes")
-    }
+	minutes, err := strconv.Atoi(parts[1])
+	if err != nil {
+		fmt.Println("Couldn't parse minutes")
+	}
 
-    duration := hours*60*60 + minutes*60
-    return duration
-}
-
-func writeJSON(contests []Contest) {
-    f, err := json.MarshalIndent(contests, "", " ")
-    if err != nil {
-        fmt.Println("Failed to JSON encode contests slice")
-    }
-
-    os.WriteFile("contests.json", f, 0644)
+	duration := hours*60*60 + minutes*60
+	return duration
 }
